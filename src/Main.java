fc921ab3418b41f6c20b5e317523a82cce4ba544
Pavel <Pahenmurman@gmail.com>
2025-03-11 22:53:47 +0300

    hw12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+// task 1
+        System.out.println("task 1");
+        for (byte i = 1; i <= 10; i++){
+            System.out.println(i);}
+        System.out.println();
+// task 2
+        System.out.println("task 2");
+        for (byte i = 10; i >=1; i--){
+            System.out.println(i); }
+        System.out.println();
+// task 3
+        System.out.println("task 3");
+        for (byte i = 0; i < 17; i =(byte) (i + 2)){
+            System.out.println(i); }
+        System.out.println();
+// task 4
+        System.out.println("task 4");
+        for (byte i = 10; i >= -10; i--){
+            System.out.println(i); }
+        System.out.println();
+// task 5
+        System.out.println("task 5");
+        for (int i = 1904; i <= 2096; i = i+4) {
+            if ( (i%100) != 0) {
+                System.out.println(i + " год является високосным");
+            }
+        }
+
     }
 }
