6898acb0e5ff731323a8ca07cf176b2203df6bbc
Pavel <Pahenmurman@gmail.com>
2025-03-12 20:15:20 +0300

    hw12

--- src/Main.java
+++ src/Main.java
@@ -24,9 +24,37 @@ public class Main {
         System.out.println("task 5");
         for (int i = 1904; i <= 2096; i = i+4) {
             if ( (i%100) != 0) {
-                System.out.println(i + " год является високосным");
-            }
+                System.out.println(i + " год является високосным"); }
         }
-
+        System.out.println();
+// task 6
+        System.out.println("task 6");
+        for (int i =1; i<=14; i++){
+            System.out.println(i*7); }
+        System.out.println();
+// task 7
+        System.out.println("task 7");
+        for (int i=1; i<=512;){
+            System.out.println(i);
+            i =i*2; }
+        System.out.println();
+// task 8
+        System.out.println("task 8");
+        int cash =0;
+        for (int i=1; i<=12; i++){
+            cash =cash+29000;
+            System.out.println("месяц "+i+", сумма накоплений равна "+cash+" рублей"); }
+        System.out.println();
+// task 9
+        System.out.println("task 9");
+        float deposit =0.0f;
+        for (int i=1; i<=12; i++){
+            deposit =deposit+(deposit*0.01f)+29000f;
+            System.out.println("месяц "+i+", сумма накоплений равна "+deposit+" рублей"); }
+        System.out.println();
+// task 10
+        System.out.println("task 10");
+        for (int i=1; i<=10; i++){
+            System.out.println("2*"+i+"="+(2*i)); }
     }
 }
